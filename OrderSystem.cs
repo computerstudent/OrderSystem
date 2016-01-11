9827ae997477621071c9444b4d561dd8702f35bb
George <aeliusdev@users.noreply.github.com>
2016-01-11 09:25:30 +0000

    Added OrderSystem Class

---
+++ OrderSystem.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Windows.Forms;
+
+public class OrderSystem 
+{
+	public List<Order> orders = new List<Order>();
+	
+	public OrderSystem ()
+	{
+		
+	}
+}
